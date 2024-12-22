3601ca5f83f7e559539c9c9b1a506529f78d362d
aszufel <32390126+aszufel@users.noreply.github.com>
2024-12-22 10:43:35 +0100

    Create __init__.py

---
+++ email_finder/__init__.py
@@ -0,0 +1,8 @@
+"""
+Email Finder - narzędzie do wyszukiwania adresów email na stronach internetowych.
+"""
+
+from .email_finder import EmailFinder
+
+__version__ = '1.0.0'
+__author__ = 'Twoje Imię'  # Zmień na swoje dane
